7ae35c147bf806f70e9b57f345dd8e318677c219
R3gret <biluganpierepaolo@gmail.com>
2025-05-04 07:55:49 +0800

    adding of cdc

--- src/components/ECCDC/ECCDCSidebar.jsx
+++ src/components/ECCDC/ECCDCSidebar.jsx
@@ -4,7 +4,8 @@ import {
   LayoutDashboard,
   List,
   Settings,
-  UserCog,
+  Users,
+  Baby
 } from "lucide-react";
 
 export default function ECCDCSidebar() {
@@ -13,7 +14,7 @@ export default function ECCDCSidebar() {
       <div className="mt-16 flex flex-col space-y-4">
         <SidebarItem to="/account-list" icon={List} label="Account List" />
         <SidebarItem to="/eccdc-manageacc" icon={Settings} label="Manage Account" />
-        <SidebarItem to="/eccdc-createacc" icon={UserCog} label="Admin Profile" />
+        <SidebarItem to="/eccdc-createacc" icon={Baby} label="ECCDC Profile" />
       </div>
     </aside>
   );

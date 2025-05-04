1f70d11242215eb1891a41b15b33cac40aea759f
R3gret <biluganpierepaolo@gmail.com>
2025-05-05 07:13:49 +0800

    admin mod

--- src/pages/CDC/Domain.jsx
+++ src/pages/CDC/Domain.jsx
@@ -314,7 +314,7 @@ const TableComponent = () => {
     const fetchStudents = async () => {
       try {
         setLoading(true);
-        const data = await apiRequest('/api/domm/att');
+        const data = await apiRequest('/api/dom/att');
         setChildOptions(
           (data.students || []).map(student => ({
             id: student.student_id,

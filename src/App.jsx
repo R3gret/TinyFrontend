18378ca5cf9490a500ed52d60a91bed239ed65a7
R3gret <biluganpierepaolo@gmail.com>
2025-11-17 21:35:12 +0800

    Fix for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/CDC/Dashboard";
 import Registration from "./pages/CDC/Registration";
